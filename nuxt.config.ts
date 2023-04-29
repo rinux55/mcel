f5b4b90897839f9e7ed15e44d68c4a4341ea3b62
Marijn Kok <marijn@funda.nl>
2023-04-29 23:40:59 +0200

    remove ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss','nuxt-chatgpt'],
-    ssr:true,
     chatgpt: {
       apiKey: process.env.OPENAI_API_KEY,
     },
